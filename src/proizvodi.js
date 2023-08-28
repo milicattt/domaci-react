c833d2beeb33a6b4fe973b59eab1ffc0d16e0326
Milica Tomasevic <milicatomasevic000@gmail.com>
2023-08-29 01:09:32 +0200

    napravljen niz proizvoda koji koristi slike

---
+++ src/proizvodi.js
@@ -0,0 +1,101 @@
+import d from'./images/d.png'
+import d1 from'./images/d1.png'
+import g from'./images/g.png'
+import g1 from'./images/g1.png'
+import gu from'./images/gu.png'
+import gu1 from'./images/gu1.png'
+import gu2 from'./images/gu2.png'
+import lv from'./images/lv.png'
+import lv1 from'./images/lv1.png'
+import lv2 from'./images/lv2.png'
+import mk from'./images/mk.png'
+import mk1 from'./images/mk1.png'
+import mk2 from'./images/mk2.png'
+import p from'./images/p.png'
+
+export const PROIZVODI=[
+    {
+        id: 1,
+        naziv: "Dior tasna roze S",
+        cena: 60000.00,
+        slika: d
+    },
+    {
+        id: 2,
+        naziv: "Dior tasna belo-roze L",
+        cena: 90000.00,
+        slika: d1
+    },
+    {
+        id: 3,
+        naziv: "Gucci tasna roze M",
+        cena: 150000.00,
+        slika: g
+    },
+    {
+        id: 4,
+        naziv: "Gucci tasna boja koze M",
+        cena: 127900.00,
+        slika: g1
+    },
+    {
+        id: 5,
+        naziv: "Guess tasna bela S",
+        cena: 16000.00,
+        slika: gu
+    },
+    {
+        id: 6,
+        naziv: "Guess tasna roza M",
+        cena: 35000.00,
+        slika: gu1
+    },
+    {
+        id: 7,
+        naziv: "Guess tasna tamno siva L",
+        cena: 40000.00,
+        slika: gu2
+    },
+    {
+        id: 8,
+        naziv: "Louis Vuitton tasna bordo S",
+        cena: 90000.00,
+        slika: lv
+    },
+    {
+        id: 9,
+        naziv: "Louis Vuitton tasna braon S",
+        cena: 100000.00,
+        slika: lv1
+    },
+    {
+        id: 10,
+        naziv: "Louis Vuitton tasna tamno siva M",
+        cena: 150000.00,
+        slika: lv2
+    },
+    {
+        id: 11,
+        naziv: "Michael Kors tasna roza L",
+        cena: 70000.00,
+        slika: mk
+    },
+    {
+        id: 12,
+        naziv: "Michael Kors tasna roza XL",
+        cena: 90000.00,
+        slika: mk1
+    },
+    {
+        id: 13,
+        naziv: "Michael Kors tasna roza XL",
+        cena: 100000.00,
+        slika: mk2
+    },
+    {
+        id: 14,
+        naziv: "Prada tasna tasna svetlo braon L",
+        cena: 150000.00,
+        slika: p
+    }
+]
